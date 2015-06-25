53943dea5c11b15902f935e324598185c7f43a99
Tong Shu Li <tongli@scripps.edu>
2015-06-25 15:37:31 -0700

    revised AUC determining function to also allow drawing of the actual ROC curve

--- src/get_AUC_value.R
+++ src/get_AUC_value.R
@@ -1,16 +1,18 @@
-# last updated 2015-06-24 tong shu li
+# last updated 2015-06-25 tong shu li
 
 # calculates the AUC value for a ROC curve
-# without actually generating a plot of the curve
-# since rpy2 cannot convert pandas dataframes
-# to R dataframes, the data frame has to be read
-# from a text file. The score_col and class_col
-# variables determine the column names to be used
-# for generating the ROC curve
+# if a filename and title are provided,
+# then the function also actually plots the curve
+
+# input is taken from a file
+
+# score_col and class_col determine the
+# column names used for generating the curve
 
 suppressMessages(library(ROCR))
 
-AUC_value <- function(fname, score_col, class_col)
+AUC_value <- function(fname, score_col, class_col,
+    curve_fname = "", title = "")
 {
     df <- read.table(fname, sep = '|', header = TRUE)
 
@@ -19,8 +21,19 @@ AUC_value <- function(fname, score_col, class_col)
 
     pred <- prediction(scores, class)
 
-    temp <- performance(pred, "auc")
-    auc <- slot(temp, "y.values")[[1]]
+    # draws a ROC curve only if the curve_filename is specified
+    if (curve_fname != "")
+    {
+        perf <- performance(pred, measure = "tpr", x.measure = "fpr")
+        png(filename = curve_fname, height = 800, width = 800, bg = "white")
+
+        plot(perf, main = title, type = "o", col = rainbow(10))
+        abline(a = "0", b = "1", lty = 2)
+        dev.off()
+    }
+
+    perf <- performance(pred, "auc")
+    auc <- slot(perf, "y.values")[[1]]
 
     return(auc)
 }
--- src/get_AUC_value.py
+++ src/get_AUC_value.py
@@ -1,15 +1,16 @@
-# last updated 2015-06-24 tong shu li
-
-# This is the Python wrapper for the R get_AUC_value.R
-# function. Since pandas dataframes can't be converted
-# to R dataframes with rpy2, we have to unfortunately
-# write the dataframe to a text file first
-
+# last updated 2015-06-25 tong shu li
+"""
+This is the Python wrapper for get_AUC_value.R
+The R function always returns the AUC value, and
+only draws the curve if the filename and title
+are specified.
+"""
 import pandas as pd
 import rpy2.robjects as robjects
 import tempfile
 
-def get_AUC_value(data_frame, score_col, class_col):
+def get_AUC_value(data_frame, score_col, class_col,
+    curve_fname = "", plot_title = ""):
     """
     Given a Pandas dataframe with the names of the
     score and class columns, calls the R function for
@@ -19,7 +20,8 @@ def get_AUC_value(data_frame, score_col, class_col):
       1. Makes a temporary file (default is in /tmp)
       2. Writes the pandas dataframe to the temporary file
       3. Calls R function using rpy2
-      4. R function reads from temporary file and calculates AUC
+      4. R function reads from temporary file and calculates AUC,
+        and optionally draws the ROC curve.
       5. Temporary file is deleted
       6. AUC value is returned to the calling Python function
     """
@@ -29,8 +31,8 @@ def get_AUC_value(data_frame, score_col, class_col):
         data_frame.to_csv(fname, sep = "|", index = False)
 
         R = robjects.r
-        R.source("get_AUC_value.R")
+        R.source("/home/toby/Research/Projects/biocreativeV/src/get_AUC_value.R")
 
-        auc = R.AUC_value(fname, score_col, class_col)
+        auc = R.AUC_value(fname, score_col, class_col, curve_fname, plot_title)
 
     return auc[0]
