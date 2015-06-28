cce060a307c592e1906f39d76fdfb7055b85f876
Tong Shu Li <tongli@scripps.edu>
2015-06-27 21:40:15 -0700

    added the option to penalize unchosen choices

--- src/aggregate_votes.py
+++ src/aggregate_votes.py
@@ -18,37 +18,36 @@ def aggregate_votes(uniq_id, data_frame, penalize_unchosen = False)
       1. Choices which are unchosen get no change to their score.
       2. Choices which are unchosen get the trust score subtracted.
     """
+    scores = defaultdict(float)
+    for idx, row in data_frame.iterrows():
+        user_choices = row["chemical_disease_relationships"].split('\n')
+        if "none_are_true" in user_choices:
+            assert len(user_choices) == 1, idx
+            for i in range(5):
+                scores["choice_{0}".format(i)] -= row["_trust"]
+        else:
+            for i in range(5):
+                choice = "choice_{0}".format(i)
+                if choice in user_choices:
+                    score[choice] += row["_trust"]
+                else:
+                    score[choice] += (-1 if penalize_chosen else 0) * row["_trust"]
 
     rel_id = dict()
-
-    # first map the ids: choice # -> id_pair
+    # map the ids: choice # -> id_pair
     for i in range(5):
         colname = "choice_{0}_ids".format(i)
         assert len(data_frame[colname].unique()) == 1
         rel_id["choice_{0}".format(i)] = data_frame.iloc[0][colname]
 
-    scores = defaultdict(float)
-    # increment each relationship pair id by the worker's trust score
-    for idx, row in data_frame.iterrows():
-        # check that none of the above does not conflict with the other choices
-        user_choices = row["chemical_disease_relationships"].split('\n')
-        if "none_are_true" in user_choices:
-            assert len(user_choices) == 1, idx
-            # vote against all other choices
-            for i in range(5):
-                scores[rel_id["choice_{0}".format(i)]] -= row["_trust"]
-        else:
-            for choice in user_choices:
-                scores[rel_id[choice]] += row["_trust"]
-
     total_trust = sum(data_frame["_trust"])
 
-    # normalize choices
+    # normalize scores
     temp = defaultdict(list)
-    for id_pair, score in scores.items():
+    for choice, score in scores.items():
         score /= total_trust
-        if id_pair != "empty":
-            temp["id_pair"].append(id_pair)
+        if rel_id[choice] != "empty":
+            temp["id_pair"].append(rel_id[choice])
             temp["normalized_score"].append(score)
 
     df = pd.DataFrame(temp)
