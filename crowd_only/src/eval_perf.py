0a163eddc82fc1538bf5df1a2520ec5e5fb6dc71
Tong Shu Li <tongli@scripps.edu>
2015-10-21 13:43:00 -0700

    Simplify portions of the analysis of crowd performance on the testset.
    Moved reusable performance analysis code to an external file. Rewrote
    the result aggregator to be much faster and cleaner.

--- crowd_only/src/eval_perf.py
+++ crowd_only/src/eval_perf.py
@@ -1,7 +1,22 @@
 # Tong Shu Li
-# Last updated: 2015-10-08
+# Last updated: 2015-10-21
+
+from collections import defaultdict
+from itertools import groupby
+import pandas as pd
 
 from .F_score import F_score
+from .mesh_filter import filter_relations
+from .data_model import Simple_Rel
+from .data_model import Ontology_ID
+
+def get_triples(dataframe):
+    TRIPLE = ["pmid", "chemical_id", "disease_id"]
+    return set(
+        dataframe[TRIPLE].apply(
+            lambda row: (int(row["pmid"]), row["chemical_id"], row["disease_id"]), axis = 1
+        )
+    )
 
 def performance(gold, predict, human_readable = False):
     """Calculates precision, recall, and F1 score.
@@ -19,7 +34,6 @@ def performance(gold, predict, human_readable = False):
     r=tp/(tp+fn)
     f=(2∙p∙r)/(p+r)
     """
-
     tp = gold & predict
     fp = predict - gold
     fn = gold - predict
@@ -36,3 +50,41 @@ def performance(gold, predict, human_readable = False):
     print("# False neg: {0}".format(len(fn)))
 
     print("Precision: {0}\nRecall: {1}\nF-score: {2}".format(precision, recall, f1))
+
+
+def official_F_score(score_column, gold_rel_set, dataframe, apply_mesh_filter = False):
+    def apply_filter(predict):
+        """Apply MeSH Ontology filter."""
+        res = set()
+
+        # group by pmid
+        temp = sorted(list(predict), key = lambda val: val[0])
+        for pmid, group in groupby(temp, lambda val: val[0]):
+            rels = [Simple_Rel(pmid, Ontology_ID(chem), Ontology_ID(dise)) for info, chem, dise in group]
+            filtered = filter_relations(rels)
+            filtered = [(rel.pmid, rel.chemical.flat_repr, rel.disease.flat_repr) for rel in filtered]
+
+            res |= set(filtered)
+
+        return res
+
+    EPSILON = 0.0000001
+
+    res = defaultdict(list)
+    for threshold in dataframe[score_column].unique():
+        sub = dataframe.query("{0} > {1} or -{2} <= {0} - {1} <= {2}".format(score_column, threshold, EPSILON))
+
+        # grab the relation ids we guessed
+        predict = get_triples(sub)
+
+        if apply_mesh_filter:
+            predict = apply_filter(predict)
+
+        precision, recall, f1 = performance(gold_rel_set, predict)
+
+        res["recall"].append(recall)
+        res["precision"].append(precision)
+        res["threshold"].append(threshold)
+        res["F_score"].append(f1)
+
+    return pd.DataFrame(res).sort("threshold").reset_index(drop = True)
