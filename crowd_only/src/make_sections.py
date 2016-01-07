eab0ff5c2bd694b46983b15fd1e307edb46885e5
Tong Shu Li <tongli@scripps.edu>
2016-01-07 15:31:24 -0800

    Fix bug which would omit the first section.
    Fix the bug which would omit the first section of an abstract from the
    result if there was no section heading for the first section (e.g. PMID
    19729346).

--- crowd_only/src/make_sections.py
+++ crowd_only/src/make_sections.py
@@ -1,6 +1,6 @@
 # Tong Shu Li
 # Created on Wednesday 2015-07-22
-# Last updated 2015-07-30
+# Last updated 2016-01-07
 """
 This program takes the highlighted abstract and
 splits it into sections based on the headings
@@ -10,32 +10,37 @@ Previously this was done by querying the PubMed
 API to determine what the section names were,
 but now the section names have been cached instead.
 """
+import os
+
 from .lingpipe.file_util import read_file
 from collections import defaultdict
 
 def create_sections(abstract):
-    fname = "data/all_uniq_section_names.txt"
+    """
+    Some section headings are suffixes of other headings (e.g., METHODS:,
+    DESIGN/METHODS:). When this occurs, both headings will be found, but we want
+    to take the longer heading.
+    """
+    fname = os.path.abspath(os.path.join("..", "data", "all_uniq_section_names.txt"))
     all_section_names = [line for line in read_file(fname)]
 
     earliest_idx = defaultdict(set)
     for heading in all_section_names:
-        start = abstract.find("{0}:".format(heading))
+        start = abstract.find("{}:".format(heading))
         if start != -1:
+            # record all headings ending at this position
             stop = start + len(heading)
             earliest_idx[stop].add(start)
 
-    positions = [len(abstract)]
+    positions = [0, len(abstract)]
     for end_pos, starts in earliest_idx.items():
         positions.append(min(starts))
-
-    if len(positions) == 1:
-        return abstract
 
     positions = sorted(positions)
 
     pieces = []
     for i in range(len(positions) - 1):
         text = abstract[positions[i] : positions[i+1]]
-        pieces.append("<p>{0}</p>".format(text))
+        pieces.append("<p>{}</p>".format(text))
 
     return "".join(pieces)
